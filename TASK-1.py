0bc621f8c990a189bd4315c2a401daf9b0d8b745
VADESADE <vetalik.selikhov@mail.ru>
2023-02-22 10:49:59 +0300

    add TASK-1.py

--- TASK-1.py
+++ TASK-1.py
@@ -3,3 +3,11 @@ userName = "Vitaliy"
 userAge = 22+1
 user = f"Name:{userName} Age:{userAge}"
 print(user)
+
+userName = list("VITALIY")
+userName2 = list("SELIKHOV")
+userName3 = list("ANDREEVICH")
+print(userName,userName2,userName3)
+
+
+
