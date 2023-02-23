7dad5ec1932c3c3ed1db9ff6933ad90166e2dc1d
VADESADE <vetalik.selikhov@mail.ru>
2023-02-23 15:35:51 +0300

    add TASK-1.py

--- TASK-1.py
+++ TASK-1.py
@@ -1,13 +1,28 @@
 #task-1
-userName = "Vitaliy"
+userName = 'Vitaliy'
 userAge = 22+1
-user = f"Name:{userName} Age:{userAge}"
-print(user)
+user = [userName,userAge]
+print (user)
+frends = ['Vitiy','Zahar','Ernest', 'Slavik']
+print(frends)
 
-userName = list("VITALIY")
-userName2 = list("SELIKHOV")
-userName3 = list("ANDREEVICH")
-print(userName,userName2,userName3)
+dict = {"Vitiy": "Developed", "Zahar": "Beatuful", "Ernest": "Sensai", "Slavik": "Stylish"}
+for a in dict.items():
+    print(a)
+
+City = set({"Krasnodar","Mihoz", "Spain", "Mihoz"})
+print(City)
+
+clock = int(input("How many hours are in dota 2?"))
+if (clock >=1000):
+    print("Archon")
+elif (clock >= 100) and (clock < 300):
+    print("Crusader")
+elif (clock >= 300) and (clock < 1000):
+    print("Guardian")
+else: (clock <= 1000)
+print("Herald")
+
 
 
 
