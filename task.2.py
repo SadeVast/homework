fcb935eb79d772494a6d72b1a0c9a7cc3955624a
VADESADE <vetalik.selikhov@mail.ru>
2023-03-03 17:55:10 +0300

    add new file task-2

--- task.2.py
+++ task.2.py
@@ -7,3 +7,19 @@ print(f'Сколько времени?{cloak}')
 f = open('text.txt')
 fd = f.read()
 print(fd)
+
+def calculator (a,b):
+    return (a+b)
+print(calculator(1, 3))
+
+def total (*numbers):
+    print(1,2,32,412,412,532,)
+print(total())
+
+
+x = 1
+def number ():
+    global x
+    x = 5
+    print(x)
+number()
